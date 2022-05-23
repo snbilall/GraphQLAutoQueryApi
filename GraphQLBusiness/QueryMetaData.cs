4b3e6073541713d2fbe739f01946a1347b6ae108
Abdurrehim Şen <abdurrehimsen@gmail.com>
2022-05-23 15:27:12 +0300

    wip

---
+++ GraphQLBusiness/QueryMetaData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GraphQLBusiness
+{
+    public class ColumnMetadata
+    {
+        public string ColumnName { get; set; }
+        public string DataType { get; set; }
+    }
+
+    public class TableMetadata
+    {
+        public string TableName { get; set; }
+        public string AssemblyFullName { get; set; }
+        public List<ColumnMetadata> Columns { get; set; }
+    }
+}
---
+++ GraphQLBusiness/ServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Business
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddGxraphQlServices(this IServiceCollection services)
+        {
+            //services
+            //.AddGraphQL(
+            //    (options, provider) =>
+            //    {
+            //        // Load GraphQL Server configurations
+            //        var graphQLOptions = Configuration
+            //            .GetSection("GraphQL")
+            //            .Get<GraphQLOptions>();
+            //        options.ComplexityConfiguration = graphQLOptions.ComplexityConfiguration;
+            //        options.EnableMetrics = graphQLOptions.EnableMetrics;
+            //        // Log errors
+            //        var logger = provider.GetRequiredService<ILogger<Startup>>();
+            //        options.UnhandledExceptionDelegate = ctx =>
+            //            logger.LogError("{Error} occurred", ctx.OriginalException.Message);
+            //    })
+            //// Adds all graph types in the current assembly with a singleton lifetime.
+            //.AddGraphTypes()
+            //// Add GraphQL data loader to reduce the number of calls to our repository. https://graphql-dotnet.github.io/docs/guides/dataloader/
+            //.AddDataLoader()
+            //.AddSystemTextJson();
+        }
+    }
+}
