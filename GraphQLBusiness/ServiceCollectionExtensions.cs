f215e4ad6bfd0b751b62363c624b76a5f536a5e8
Abdurrehim Şen <abdurrehimsen@gmail.com>
2022-05-31 21:13:01 +0300

    query api

--- GraphQLBusiness/ServiceCollectionExtensions.cs
+++ GraphQLBusiness/ServiceCollectionExtensions.cs
@@ -1,44 +1,15 @@
 ﻿using HotChocolate;
-using HotChocolate.AspNetCore;
-using HotChocolate.AspNetCore.Playground;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using GraphQLBusiness;
 
 namespace Business
 {
     public static class ServiceCollectionExtensions
     {
-        public static void AddGxraphQlServices(this IServiceCollection services)
+        public static void AddGraphQlServices(this IServiceCollection services)
         {
             services.AddGraphQLServer()
                 .AddQueryType<Query>();
-        }
-
-        public static void AddGxraphQl(this IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UsePlayground(new PlaygroundOptions
-                {
-                    QueryPath = "/api",
-                    Path = "/playground"
-                });
-            }
-            app.UseGraphQL("/api");
         }
     }
 }
---
+++ GraphQLQueryApi/Startup.cs
@@ -0,0 +1,46 @@
+using Business;
+using Core.Extensions;
+using DataLayer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace GraphQLQueryApi
+{
+    public class Startup
+    {
+        public Startup(IWebHostEnvironment env)
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder();
+            builder.BuildConf(env);
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDataLayers(Configuration);
+            services.AddGraphQlServices();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGraphQL();
+            });
+        }
+    }
+}
